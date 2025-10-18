cf05238dc0716297ef3ad748d3dc553a6206f890
ram-itdivision <ram@aditya.ac.in>
2025-10-18 16:27:35 +0530

    thirdsem fee updated

--- src/app/utilities/config.ts
+++
@@ -1,8 +0,0 @@
-import { apiURL } from "src/environments/environment.development";
-
-export  const apiUrls: any = {
-
-    usermaster: {
-        rootEndPoint: `${apiURL}/users`
-    }
-};
--- src/app/utilities/socialauth.ts
+++
@@ -1,5 +0,0 @@
-
- export const googleclientID = 'dfdfdfdfd'
-
-//*************localhost:4200*************
-//export const googleclientID = '3dfdfdfdfd'
